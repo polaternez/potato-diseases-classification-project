140517735f50790d81dbe0fca805646740f2214d
Polat Ernez <polat_ernez@windowslive.com>
2024-02-06 13:34:05 +0300

    main.py added

---
+++ main.py
@@ -0,0 +1,57 @@
+from src.potatodisease import logger
+from src.potatodisease.pipeline.stage01_data_ingestion import DataIngestionTrainingPipeline
+from src.potatodisease.pipeline.stage02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from src.potatodisease.pipeline.stage03_training import ModelTrainingPipeline
+from src.potatodisease.pipeline.stage04_evaluation import EvaluationPipeline
+
+
+# Data ingestion
+STAGE_NAME = "Data Ingestion stage"
+
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+# Prepare base model
+STAGE_NAME = "Prepare base model"
+
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+# Training
+STAGE_NAME = "Training"
+
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_trainer = ModelTrainingPipeline()
+   model_trainer.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+# Evaluation
+STAGE_NAME = "Evaluation stage"
+
+try:
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_evalution = EvaluationPipeline()
+   model_evalution.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
