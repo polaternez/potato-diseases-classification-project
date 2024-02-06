22adf3074683df207888eac780ac47ca1f31bf11
Polat Ernez <polat_ernez@windowslive.com>
2024-02-06 13:26:50 +0300

    model evaluation pipeline added

--- src/potatodisease/config/configuration.py
+++ src/potatodisease/config/configuration.py
@@ -7,7 +7,8 @@ from potatodisease.entity.config_entity import (
     DataIngestionConfig,
     PrepareBaseModelConfig,
     PrepareCallbacksConfig,
-    TrainingConfig
+    TrainingConfig,
+    EvaluationConfig
 )
 
 
@@ -94,5 +95,18 @@ class ConfigurationManager:
         )
 
         return training_config
+    
+    # Validation config
+    def get_validation_config(self) -> EvaluationConfig:
+        eval_config = EvaluationConfig(
+            path_of_model=Path("artifacts/training/model.h5"),
+            training_data=Path("artifacts/data_ingestion/potato-diseases-images"),
+            
+            all_params=self.params,
+            params_image_size=self.params.IMAGE_SIZE,
+            params_batch_size=self.params.BATCH_SIZE
+        )
+        
+        return eval_config
 
     
